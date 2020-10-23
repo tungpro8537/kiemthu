5497a59b4980a9102b7c09a958e121a7a4013a85
Callme Tùn <tungpro8537@gmail.com>
2020-10-23 22:22:55 +0700

    Initial commit

---
+++ inventory/InventorySystem_C#/InventorySystem1.0/InventorySystem1.0/ktra.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InventorySystem1._0
+{
+    public class ktra
+    {
+        public bool empty(string name, string pass, string user)
+        {
+            if (name == "" || pass == "" || user == "")
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ inventory/InventorySystem_C#/InventorySystem1.0/testunit1/Class1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NUnit.Framework;
+using InventorySystem1._0;
+
+namespace testunit1
+{
+    [TestFixture]
+    public class Class1
+    {
+        frmUsers testUser = new frmUsers();
+        ktra kt = new ktra();
+        [Test]
+        public void ktr0a()
+        {
+            //bool x = testUser.empty("", "a", "b");
+            bool x = kt.empty("", "a", "b");
+
+            Assert.IsTrue(x);
+        }
+    }
+}
